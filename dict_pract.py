76d966f4326dc1e2b8bbb0c61d1a5151921aa238
Jonalds456 <jonalds1234567@gmail.com>
2026-01-19 13:36:06 +0100

    Added dict_pract and list_prac

---
+++ dict_pract.py
@@ -0,0 +1,13 @@
+student = {      
+           "name": "Jonathan Akinola",
+           "age": 20,
+           "course": "Software Engineering"
+           }
+
+print(student)
+print(student["name"])
+print(student["course"])
+
+student["Level"] = "400"
+
+print(student)
---
+++ list_prac.py
@@ -0,0 +1,12 @@
+courses = ["Maths" , "Software Eng" , "computer sci" ]
+print(courses)
+
+print(courses[0])
+print(courses[2])
+print(courses[-1])
+
+courses.append("cyber sec")
+courses.remove("Maths")
+
+print(courses)
+print(courses.count("r"))
