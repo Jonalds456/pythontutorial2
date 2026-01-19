d85d15bb39f0e3088b82b557a1804b4c14d37626
Jonalds456 <jonalds1234567@gmail.com>
2026-01-19 10:38:50 +0100

    Added string practice file

---
+++ string.py/practice.py
@@ -0,0 +1,11 @@
+name = "Jonathan"
+course = "Software engineering"
+
+
+info = f"My name is {name} and I am studying {course}"
+
+print(info)
+
+sentence = "I am learning python programming."
+
+print(sentence.upper())
