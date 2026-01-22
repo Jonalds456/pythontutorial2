1eab55e3541b687e2bb99b2b16fe161c245ba668
Jonalds456 <jonalds1234567@gmail.com>
2026-01-22 06:53:41 +0100

    add int_float inside string

---
+++ int_float.py
@@ -0,0 +1,24 @@
+num = 3 
+
+num_2 = 4.35
+
+print(type(num_2))
+print(type(num))
+
+
+sum = 3 + 4 
+
+print(sum)
+
+
+print(round(num_2))
+
+diff = num_2 *( num - sum)
+
+print(diff)
+
+print(abs(diff))
+
+
+
+
